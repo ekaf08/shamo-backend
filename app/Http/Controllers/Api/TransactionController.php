11fbfd5ed11b6bbd5c72082e6c74c2ee6492b878
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2025-06-02 13:59:25 +0700

    API  cek out

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Transactions;
+use App\Models\TransactionsItem;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -42,6 +43,39 @@ class TransactionController extends Controller
         return ResponseFormatter::success(
             $transaction,
             'Transaksi ditemukan2'
+        );
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'items'          => 'required|array',
+            'items.*.id'     => 'exists:product,id',
+            'total_price'    => 'required',
+            'shipping_price' => 'required',
+            'status'         => 'required|in:PENDING, SUCCESS. CANCELLED, FAILED, SHIPPING, SHIPPED'
+        ]);
+
+        $transaction = Transactions::create([
+            'user_id'        => auth()->user()->id,
+            'address'        => $request->address,
+            'total_price'    => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status'         => $request->status
+        ]);
+
+        foreach ($request->items as $product) {
+            TransactionsItem::create([
+                'user_id'        => auth()->user()->id,
+                'product_id'     => $product['id'],
+                'transaction_id' => $transaction->id,
+                'quantity'       => $product['quantity']
+            ]);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->load('items.product'),
+            'Transaksi Berhasil'
         );
     }
 }
